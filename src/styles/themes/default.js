3f67ad329f9dbefee72d1fe0abb23e7fa061c224
adrielqueiroz2006 <adrielqueiro@gmail.com>
2025-06-29 10:02:25 -0300

    feat(style): add default theme

---
+++ src/styles/themes/default.js
@@ -0,0 +1,16 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#F4F4F5',
+  'gray-200': '#ECECEE',
+  'gray-300': '#D4D4D8',
+  'gray-500': '#8D8D99',
+  'gray-600': '#50505C',
+  'gray-800': '#27272A',
+  'gray-900': '#09090B',
+
+  'red-500': '#F62121',
+  'red-700': '#CF1414',
+
+  'blue-500': '#3A83F3',
+}
