00b39c2e7c40bb796a81e82f7e04c8d72092c4d3
adrielqueiroz2006 <adrielqueiro@gmail.com>
2025-06-29 17:10:48 -0300

    feat(routes): add new routes to application

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
+
 import { Login } from './pages/Login'
+import { Home } from './pages/Home'
+import { Customers } from './pages/Customers'
+import { Products } from './pages/Products'
+import { Orders } from './pages/Orders'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/clientes" element={<Customers />} />
+      <Route path="/produtos" element={<Products />} />
+      <Route path="/pedidos" element={<Orders />} />
     </Routes>
   )
 }
