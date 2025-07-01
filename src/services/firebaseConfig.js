61959e6a20c160ac4e7e2eeef666ea702e9727c1
adrielqueiroz2006 <adrielqueiro@gmail.com>
2025-07-01 13:52:59 -0300

    chore: configure firebase

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXV2qgPj4SioX2_yUkXhXBUsfoccADdWg',
+  authDomain: 'red-market-project.firebaseapp.com',
+  projectId: 'red-market-project',
+  storageBucket: 'red-market-project.firebasestorage.app',
+  messagingSenderId: '357437164496',
+  appId: '1:357437164496:web:2ef418248f6e99480e07d5',
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
